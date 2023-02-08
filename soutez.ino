404ac872a8c2901c0eb9840d5e4a7d990a720476
marcel <marcelgodovsky@seznam.cz>
2023-02-08 14:08:43 +0100

    first commit

---
+++ soutez.ino
@@ -0,0 +1,42 @@
+#define bits 10
+#define minNum 0
+#define maxNum 1024
+int ledBar[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+void setup() {
+  for (int i = 0; i < bits; i++) {
+    pinMode(ledBar[i], OUTPUT);
+    digitalWrite(ledBar[i], LOW);
+  }
+  Serial.begin(9600);
+  Serial.println("Zadejte číslo v intervalu <0,1023>:  ");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String decNum = Serial.readString();
+    int decNumInt = decNum.toInt();
+    if (decNumInt >= minNum && decNumInt < maxNum) {
+      String binNum = String(decNumInt, BIN);
+      int leng = binNum.length();
+      for (int i = 0; i < bits - leng; i++) {
+        binNum = "0" + binNum;
+      }
+      Serial.print(decNum);
+      Serial.print(" = ");
+      Serial.println(binNum);
+      for (int i = 0; i < bits; i++) {
+        if (binNum[i] == '1') {
+          digitalWrite(ledBar[i], HIGH);
+        }
+        if (binNum[i] == '0') {
+          digitalWrite(ledBar[i], LOW);
+        }
+      }
+    }
+    else {
+      Serial.println("Zadali jste číslo mimo rozsah.");
+      Serial.println("Zadejte číslo v intervalu <0,1023>:  ");
+    }
+  }
+}
